a1404cdf784b0c20b51c48cdfc7dccb65c5f505a
Alireza Ahmadi far <145385348+Alireza3181@users.noreply.github.com>
2025-02-11 14:32:55 +0330

    اول‌بینی

--- (87) اول‌بینی/اول‌بینی.java
+++ (87) اول‌بینی/اول‌بینی.java
@@ -1,32 +1,30 @@
 import java.util.Scanner;
 
-public class Main
-{
-    public static void main (String[] args)
-    {
+public class Main {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int nn [] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
-        int a, b, comma=0;
+        int a = sc.nextInt();
+        int b = sc.nextInt();
 
-        do {
+        boolean first = true;
 
-            a=sc.nextInt();
-            b=sc.nextInt();
-
-        }while((a<1 || a>200) || (b<1 || b>200 || b<a));
-
-        for(int i=0 ; i < nn.length; i++)
-        {
-            if(nn[i]>a && nn[i]<b)
-            {
-                if(comma!=0)
-                    System.out.print(',');
-                
-                System.out.print(nn[i]);
-                comma++;
+        for (int i = a + 1; i < b; i++) {
+            if (isPrime(i)) {
+                if (!first) {
+                    System.out.print(",");
+                }
+                System.out.print(i);
+                first = false;
             }
         }
+    }
 
+    static boolean isPrime(int n) {
+        if (n < 2) return false;
+        for (int i = 2; i * i <= n; i++) {
+            if (n %i == 0) return false;
+        }
+        return true;
     }
 }
