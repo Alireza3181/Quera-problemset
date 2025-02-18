e72f9b9eb3e7fa4a3bf75208a4090e41d7cbe58a
Alireza Ahmadi far <145385348+Alireza3181@users.noreply.github.com>
2025-02-18 21:49:41 +0330

    Create نجات دامغان.c

---
+++ نجات دامغان/نجات دامغان.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    if (n ==1) 
+    {
+        printf("1\n");
+    } else if (n <=3) 
+    {
+        printf("1\n");
+    } else 
+    {
+        printf("%d\n",n - 2);
+    }
+    return 0;
+}
